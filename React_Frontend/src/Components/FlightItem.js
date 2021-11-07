4d2c6570b39807123cff235cee2e5793249a3776
Khaled Romeh <khaledmromeh@gmail.com>
2021-11-08 01:34:25 +0200

    Re-Introducing Modals
    - Hopefully this one works for good (if it does reward yourself Khaled)
    - Also fixed a 'memory leak'....yeah no idea what that is
    - installed react-bootstrap module

--- React_Frontend/src/Components/FlightItem.js
+++ React_Frontend/src/Components/FlightItem.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
+import {Button, Modal} from 'react-bootstrap';
 
 const Router = require('react-router-dom');
+
 
 
 const Link = Router.Link
@@ -44,21 +46,31 @@ class FlightItem extends Component{
                     <li>ToAirport: {this.props.flight.ToAirport} </li>
                     <li>Terminal: {this.props.flight.Terminal} </li>
                     </ul>
-                    {/* <button className="btn btn-danger btn-sm ml-1" hidden={hidden}>
-                        <a data-toggle="modal" data-target="#deletemodal">Delete</a>
-                    </button> */}
-
-                    {/* <button className='btn btn-outline-primary' hidden={hidden}><Link to = {updateLink} >Update</Link></button> */}
+                    
                     <Link className='btn btn-success' hidden={hidden} to = {updateLink} >Update</Link>
                     <button className='btn btn-danger' hidden={hidden} onClick={()=>{this.setState({showPop: true})}}>Delete</button>
-                    {
+                    <Modal show={this.state.showPop} onHide={()=>{this.setState({showPop: false})}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Confirm Deletion</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>Are you sure you want to delete this Flight?</Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={()=>{this.setState({showPop: false})}}>
+                                Close
+                            </Button>
+                            <Button variant="danger" onClick={()=>{this.setState({showPop: false});this.df()}}>
+                                Confirm
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                    {/* {
                         this.state.showPop &&
                         <div>
                             <h5>Are you sure?</h5>
                             <button className='btn btn-danger' hidden={hidden} onClick={this.df}>Delete</button>
                             <button className='btn btn-secondary' hidden={hidden} onClick={()=>{this.setState({showPop: false})}}>Cancel</button>
                         </div>
-                    }
+                    } */}
                 </li>
                 <br/>
                 
