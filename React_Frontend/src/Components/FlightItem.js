e72acdb2a19ff9e9101e576fcf64e5db25b42cec
Khaled Romeh <khaledmromeh@gmail.com>
2021-11-07 19:35:16 +0200

    Removed modal

--- React_Frontend/src/Components/FlightItem.js
+++ React_Frontend/src/Components/FlightItem.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 const Router = require('react-router-dom');
 
+
 const Link = Router.Link
 
 class FlightItem extends Component{
@@ -8,13 +9,16 @@ class FlightItem extends Component{
     
     constructor(props){
         super(props);
+        this.state ={
+            showPop: false
+        }
         this.df = this.df.bind(this);
     }
 
     df(){
+        console.log(this.props.flight);
         this.props.deleteFlight(this.props.flight._id);
     }
-
 
     render(){
         let hidden = '';
@@ -22,33 +26,16 @@ class FlightItem extends Component{
             hidden = true
         };
         const updateLink = '/admin/flight/update/'+this.props.flight._id;
-
+        const date = new Date(this.props.flight.FlightDate);
+        // console.log(date);
         return(
             <div>
-                <div id="deletemodal" className="modal fade" role="dialog">
-                    <div className="modal-dialog modal-lg" role="content">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h4 className="modal-title">Delete </h4>
-                                <button type="button" className="close" data-dismiss="modal">&times;</button>
-                            </div>
-                            <div className="modal-body">
-                                    <div className="form-row">
-                                        <span>Are you sure you want to delete?</span>
-                                        <button type="button" className="btn btn-secondary btn-sm ml-auto" data-dismiss="modal">Cancel</button>
-                                        <button className='btn btn-danger' data-dismiss="modal" onClick={this.df}>Delete</button>
-                                    </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>    
-
                 <li>
                 <ul>
                     <li>FlightNumber: {this.props.flight.FlightNumber}</li>
                     <li>Departure: {this.props.flight.Departure.Hours}:{this.props.flight.Departure.Minutes} {this.props.flight.Departure.Period}</li>
                     <li>Arrival: {this.props.flight.Arrival.Hours}:{this.props.flight.Arrival.Minutes} {this.props.flight.Arrival.Period} </li>
-                    <li>FlightDate: {this.props.flight.FlightDate} </li> 
+                    <li>FlightDate: {date.getDate()}-{date.getMonth()+1}-{date.getFullYear()} </li> 
                     <li>EconomySeat: {this.props.flight.EconomySeats} </li>
                     <li>BusinessSeats: {this.props.flight.BusinessSeats} </li>
                     <li>FirstClassSeats: {this.props.flight.FirstClassSeats} </li>
@@ -56,14 +43,21 @@ class FlightItem extends Component{
                     <li>ToAirport: {this.props.flight.ToAirport} </li>
                     <li>Terminal: {this.props.flight.Terminal} </li>
                     </ul>
-                    <button className="btn btn-danger btn-sm ml-1" hidden={hidden}>
+                    {/* <button className="btn btn-danger btn-sm ml-1" hidden={hidden}>
                         <a data-toggle="modal" data-target="#deletemodal">Delete</a>
-                    </button>
+                    </button> */}
 
-                    <button type='update' className='btn btn-outline-primary' hidden={hidden}><Link to = {updateLink} >Update</Link></button>
-                    
-                    {/* <button className='btn btn-danger' onClick={this.df}>Delete</button> */}
-
+                    {/* <button className='btn btn-outline-primary' hidden={hidden}><Link to = {updateLink} >Update</Link></button> */}
+                    <Link className='btn btn-success' hidden={hidden} to = {updateLink} >Update</Link>
+                    <button className='btn btn-danger' hidden={hidden} onClick={()=>{this.setState({showPop: true})}}>Delete</button>
+                    {
+                        this.state.showPop &&
+                        <div>
+                            <h5>Are you sure?</h5>
+                            <button className='btn btn-danger' hidden={hidden} onClick={this.df}>Delete</button>
+                            <button className='btn btn-secondary' hidden={hidden} onClick={()=>{this.setState({showPop: false})}}>Cancel</button>
+                        </div>
+                    }
                 </li>
                 <br/>
             </div>
